bd0c40e99b649b185ba83ca89b4a80a45ed27d25
Wander Grevink <wander.grevink@gmail.com>
2025-05-24 15:44:04 +0200

    vibrate

--- src/routes/wordle/+page.svelte
+++ src/routes/wordle/+page.svelte
@@ -38,6 +38,9 @@ function handleKey(key) {
       const valid = game.enter(currentGuess.split(''));
       if (!valid) {
         badGuess = true;
+        if (browser && navigator.vibrate) {
+          navigator.vibrate(200);
+        }
         return;
       }
       saveGame();
