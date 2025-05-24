ad08655b99bf499b3e62cecbae2030a022a7c76b
Wander Grevink <wander.grevink@gmail.com>
2025-05-24 14:49:57 +0200

    no scalable

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,8 @@
   import '../app.css';
 </script>
 
+<svelte:head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+</svelte:head>
+
 <slot /> 
